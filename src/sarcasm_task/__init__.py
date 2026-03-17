e2b27d012a7c5bf6c9aa9ca4ff941177d67b5ddb
Christopher Banerji <114677863+CRSBanerji@users.noreply.github.com>
2026-03-17 19:45:14 +0000

    Delete src/sarcasm_task directory

--- src/sarcasm_task/__init__.py
+++
@@ -1,3 +0,0 @@
-from .experiment import main, run_one_fold, run_experiment, save_results, summarize_results
-
-__all__ = ["main", "run_one_fold", "run_experiment", "save_results", "summarize_results"]
